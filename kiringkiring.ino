147509e478cf5c1d96f2694889c7a38dea93edfe
fixgramwork <kimminje661@outlook.kr>
2024-06-24 16:59:34 +0900

    3rd commit

--- kiringkiring.ino
+++ kiringkiring.ino
@@ -1,87 +1,117 @@
 #include <Wire.h>
-#include <LiquiCrystal_I2C.h>
-#include <DS1302.h>
-#include <string.h>
+#include <LiquidCrystal_I2C.h>
+#include <I2C_RTC.h>
 
-LiquiCrystal_I2C lcd(0x27,20,4); //A4, A5
+static DS1307 RTC;
+LiquidCrystal_I2C lcd(0x27, 16, 2); // I2C address 0x27, 16 columns and 2 rows
 
-void setup() {
-  rtc.halt(false);
-  rtc.writeProtect(false);
-  Serial.begin(115200);
-  rtc.setDOW(TUESDAY); //요일
-  rtc.setTime(14,10,0); //시간
-  rtc.setDate(11,6,2024); //날짜
+void setup()
+{
+  Serial.begin(9600);
+  RTC.begin();
 
-  pidMode(0,INPUT);//위
-  pidMode(1,INPUT);//아래
-  pinMode(2,INPUT);//좌
-  pinMode(3,INPUT);//우
+  lcd.init();
+  lcd.backlight();
 
-  lcd.init(); //초기화
-  lcd.backlight(); //불 켜기
+  RTC.setHourMode(CLOCK_H12);
+  // RTC.setHourMode(CLOCK_H24);
 
-  lcd.setCursor(0,0); //1번째 줄
-  lcd.serCursor(0,1); //2번째 줄
-  lcd.setCursor(0,2); //3번째 줄
-  lcd.setCursor(0,3); //4번째 줄
-  
+  if (RTC.getHourMode() == CLOCK_H12)
+  {
+     RTC.setMeridiem(HOUR_PM);
+  }
+
+  // Initialize RTC time and date
+  RTC.setWeek(1);
+  RTC.setDate(22, 07, 21);
+  RTC.setTime(23, 00, 00);
+
+  // Set pin modes
+  pinMode(5, INPUT); // 날짜
+  pinMode(6, INPUT); // 시간표
+  pinMode(7, INPUT); // 캘린더
 }
-void loop() {
-  int readhome = digitalRead(0);
-  int readcale = digitalRead(1);
-  int readtime = digitalRead(2);
 
-  //날짜
-  if(readhome == 1){
-    lcd.setCursor(0,0);
-    lcd.print(rtc.getDateStr());
-    lcd.print(rtc.getDOWStr());
-    lcd.setCursor(0,1);
-    lcd.print(rtc.getTime());
-    lcd.setCursor(0,2);
-    lcd.print("TYPE : Date");
-    lcd.setCursor(0,3);
-    delay(1000);
-  }
-//캘린터
-  if(readcale == 1){
+void loop()
+{
+  // Read pin states
+  int readDate = digitalRead(5);
+  int readSchedule = digitalRead(6);
+  int readCalendar = digitalRead(7);
 
+  if (readDate == HIGH) {
+    displayDate();
+  } else if (readSchedule == HIGH) {
+    displaySchedule();
+  } else if (readCalendar == HIGH) {
+    displayCalendar();
   }
 
-//시간표
-  if(readtime == 1){
-    lcd.setCursor(0,0);
-    if(strcmp(rtc.getDOWStr == "MONDAY"))
-      lcd.print("국|수|사|과|영|프|전|체");
-    else if(strcmp(rtc.getDOWStr == "TUESDAY"))
-      lcd.print("국|수|사|과|영|프|전|체");
-    else if(strcmp(rtc.getDOWStr == "WENSDAY"))
-      lcd.print("국|수|사|과|영|프|전|체");
-    else if(strcmp(rtc.getDOWStr == "TURSDAY"))
-      lcd.print("국|수|사|과|영|프|전|체");
-    else if(strcmp(rtc.getDOWStr == "FRIDAY"))
-      lcd.print("국|수|사|과|영|프|전|체");
-    else if(strcmp(rtc.getDOWStr == "SATURDAY"))
-      lcd.print("쉬는날");
-    else
-      lcd.print("쉬는날");
+  delay(1000); // Delay to avoid bouncing
+}
+
+void displayDate() {
+  lcd.clear();
+  lcd.setCursor(0, 0);
+  lcd.print("Date: ");
+  lcd.print(RTC.getYear());
+  lcd.print("/");
+  lcd.print(RTC.getMonth());
+  lcd.print("/");
+  lcd.print(RTC.getDay());
+
+  lcd.setCursor(0, 1);
+  lcd.print("Time: ");
+  lcd.print(RTC.getHours());
+  lcd.print(":");
+  lcd.print(RTC.getMinutes());
+  lcd.print(":");
+  lcd.print(RTC.getSeconds());
+  lcd.print(" ");
+  if (RTC.getHourMode() == CLOCK_H12) {
+    switch (RTC.getMeridiem()) {
+      case HOUR_AM :
+        lcd.print("AM");
+        break;
+      case HOUR_PM :
+        lcd.print("PM");
+        break;
+    }
   }
+}
 
+void displaySchedule() {
+  lcd.clear();
+  lcd.setCursor(0, 0);
+  switch (RTC.getWeek()) {
+    case 2: // MONDAY
+      lcd.print("MON: 국|수|사|과|영|프|전|체");
+      break;
+    case 3: // TUESDAY
+      lcd.print("TUE: 국|수|사|과|영|프|전|체");
+      break;
+    case 4: // WEDNESDAY
+      lcd.print("WED: 국|수|사|과|영|프|전");
+      break;
+    case 5: // THURSDAY
+      lcd.print("THU: 국|수|사|과|영|프|전|체");
+      break;
+    case 6: // FRIDAY
+      lcd.print("FRI: 국|수|사|과|영|프|전|체");
+      break;
+    case 7: // SATURDAY
+    case 1: // SUNDAY
+      lcd.print("WEEKEND: 쉬는날");
+      break;
+  }
+}
 
-  /*Serial.print(rtc.getDOWStr());
-  Serial.print(" ");
-
-  Serial.print(rtc.getDateStr());
-  Serial.print(" -- ");
-
-  Serial.println(rtc.getTimeStr());
-
-  delay(1000);*/
-
-  //날짜가 되었을때 초기화
-  if(!strcmp(rtc.getDateStr(),"01,01,2000"))
-    rtc.writeProtect(false);
-  else
-    rtc.writeProtet(true);
+void displayCalendar() {
+  lcd.clear();
+  lcd.setCursor(0, 0);
+  lcd.print("Year: ");
+  lcd.print(RTC.getYear());
+  lcd.setCursor(0, 1);
+  lcd.print("Calendar Function");
+  // Implement calendar display function if needed
 }
